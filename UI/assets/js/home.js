a67205c887ec4509f48ecf902585c5fbc252c2dc
Okosun Valentine <valenstical@gmail.com>
2018-11-25 11:17:49 +0100

    Complete admin page

--- UI/assets/js/home.js
+++ UI/assets/js/home.js
@@ -1,6 +1,8 @@
 const POPULAR_REPORTS=['corruption', 'election rigging', 'police extortion', 'power outage', 'Flooding', 'abandoned projects', 'pipeline leakage', 'bad roads'];
 
-let menuHidable=true; let hash='';
+let menuHidable=true; 
+const dialogDetails=new Dialog("#dialogDetails",120); 
+const dialogStatus=new Dialog("#dialogStatus",121);
 
 function iteratePopularReports(){
     const len=POPULAR_REPORTS.length;
@@ -36,7 +38,6 @@ function login(){
    return false;   
 }
 
-
 function register(){
    return login();
 }
@@ -55,61 +56,91 @@ function getReports(element,status){
     $(element).addClass('active');
 }
 
-function showDialog(id){
-    hash=id;
-    location.hash=hash; 
-    $(hash).show(0,function(){
-        $(this).find('.dialog-content').addClass('dialog-shown');
-    }).scrollTop(0);
-    $('body').addClass('no-scroll');
+function showReportDetails(element,id){
+    //Fetch records and populate dialog 
+    dialogDetails.showDialog();
 }
 
-function hideDialog(){    
-    $(hash).find('.dialog-content').removeClass('dialog-shown').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function() {
-    $(hash).hide();
-    $('body').removeClass('no-scroll');
-    hash='';
-    history.replaceState(null, null, ' '); 
-  });    
+function chooseStatus(){
+    dialogStatus.showDialog();
 }
 
-function showReportDetails(row){
-    //Fetch records
-    showDialog($(row).attr('data-target'));
+function changeStatus(){
+    dialogStatus.hideDialog();
+    return false;
 }
+
+function deleteReport(){}
 
 function init(){
     iteratePopularReports();
 }
 
-
-$('.dropdown>a').click(function(event){
+$('.dropdown>a').click(()=>{
     $(this).next().slideToggle('fast').addClass('showing');
-    event.stopPropagation();
 });
 
 
-$(document).click(function() {   
+$(window).click(()=> { 
     $('.showing').slideUp(100).removeClass('showing');
+    $('.toggle.shown').removeClass('shown');
+    
 });
 
-$('[data-share-page]').each(function(){
+$('[data-share-page]').each(()=>{
     let url=$(this).prop('href').replace('{url}',encodeURI(location.href));
     $(this).prop('href',url);
 });
 
-$('.dialog').click(function(){
-    hideDialog();
-});
 
-$('.dialog .dialog-content').click(function(event){
+
+
+$('[data-toggle]').click(function(event){
+    $(this).parent().toggleClass('shown');
     event.stopPropagation();
 });
 
-$(window).bind('hashchange',function(){
-    if (location.hash===''&&hash!=='') {
-        hideDialog();
-    }
-});
+
+
+function Dialog(id,zIndex){
+    this.id=id;
+    this.zIndex=zIndex;
+    
+    $(this.id).css({"z-index":zIndex});
+    
+    this.showDialog= ()=>{
+    location.hash=this.id; 
+    $(this.id).show(0,()=>{
+        $(this.id).addClass('shown').scrollTop(0);
+        $(window).addClass('no-scroll');
+    });
+    };
+
+    this.hideDialog=()=>{ 
+    $(this.id).removeClass('shown').find('.shown').removeClass('shown'); // Hide dropdown menu in Dialog if opened
+   
+    $(this.id).find('.dialog-content').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',()=> {
+        $(this.id).hide();    
+        $(window).removeClass('no-scroll');
+         history.replaceState(null, null, ' ');
+    });
+     };
+     
+    $(window).bind('hashchange',()=>{
+        if (location.hash==='') {
+             this.hideDialog();   
+        }
+    });
+    
+        //Hide dialogs when click on overlay
+    $(this.id).find('.dialog-overlay').click(()=>{
+         this.hideDialog();
+    });
+    
+    $(this.id).find('.close').click(()=>{
+         this.hideDialog();
+    });    
+}
+
 
 init();
