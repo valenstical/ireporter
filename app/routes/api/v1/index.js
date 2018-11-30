4defcbd3c12cb9a909f6eaa2450234f4f8e2875f
Okosun Valentine <valenstical@gmail.com>
2018-11-30 20:18:35 +0100

    Force travis ci build

--- app/routes/api/v1/index.js
+++ app/routes/api/v1/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.send('Welcome to iReporter v1 API');
+  res.send('Welcome to iReporter v1 API. Remove this');
 });
 
 module.exports = router;
