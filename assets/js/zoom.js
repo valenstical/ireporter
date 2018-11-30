ecaef413e72ed39aff3f94cadda1e8ee09e7b170
Okosun Valentine <valenstical@gmail.com>
2018-11-30 12:54:49 +0100

    Fix javascript codes to meet eslint standard

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,65 +1,65 @@
+function setSize() {
+  const windowWidth = $(window).width();
+  const windowHeight = $(window).height();
 
-let close=true;     
+  const width = $('.zoom>div').outerWidth();
 
-$(window).resize(function(){
-    setSize();  
-});    
+  const left = (windowWidth - width) / 2;
 
-$("[data-zoom]").click(function(e){
-    e.preventDefault();
-    let media=$(this).prop("href");
-    
-    $(".zoom").fadeIn(0,function(){
-    $(".zoom iframe").prop("src",media);
-       $('.zoom>div').css({"-moz-transform":"scale(1,1)","-webkit-transform":"scale(1,1)","-o-transform":"scale(1,1)","transform":"scale(1,1)"});
-        setSize(); 
+  let height = width / 1.57;
+  let top = windowHeight - height;
+
+  if (top < 0) {
+    height = windowHeight - 50;
+  }
+
+  top = (windowHeight - height) / 2;
+  $('.zoom>div').css({ left: `${left}px`, top: `${top}px`, height: `${height}px` });
+}
+let close = true;
+
+$(window).resize(() => {
+  setSize();
+});
+
+$(document).on('click', '[data-zoom]', (e) => {
+  e.preventDefault();
+  const media = $(e.currentTarget).prop('href');
+
+  $('.zoom').fadeIn(0, () => {
+    $('.zoom iframe').prop('src', media);
+    $('.zoom>div').css({
+      '-moz-transform': 'scale(1,1)', '-webkit-transform': 'scale(1,1)', '-o-transform': 'scale(1,1)', transform: 'scale(1,1)',
     });
-    $(".zoom aside").show();
+    setSize();
+  });
+  $('.zoom aside').show();
 });
 
-function setSize(){
-    let windowWidth=$(window).width();
-    let windowHeight=$(window).height();
-    
-    let width=$('.zoom>div').outerWidth();
-    
-    let left=(windowWidth-width)/2;        
-    
-    let height=width/1.57;
-    let top=windowHeight-height;
-    
-    if (top<0) {
-     height=windowHeight-50;
-    }
-    
-     top=(windowHeight-height)/2; 
-     $('.zoom>div').css({"left":left+"px","top":top+"px","height":height+"px"});   
+function closeVideo() {
+  $('.zoom>div').css({
+    '-moz-transform': 'scale(0,0)', '-webkit-transform': 'scale(0,0)', '-o-transform': 'scale(0,0)', transform: 'scale(0,0)',
+  });
+  $('.zoom').fadeOut(400);
+  $('.zoom iframe').prop('src', '');
 }
 
-function closeVideo(){
-   
-    $('.zoom>div').css({"-moz-transform":"scale(0,0)","-webkit-transform":"scale(0,0)","-o-transform":"scale(0,0)","transform":"scale(0,0)"});       
-     $(".zoom").fadeOut(400);
-    $(".zoom iframe").prop("src","");
-    
+function finishLoading() {
+  $('.zoom aside').hide();
 }
 
-function finishLoading(){
-   $(".zoom aside").hide();
-}
-
-$("body").on("mouseenter",".zoom>div",function(){
-       close=false; 
+$('body').on('mouseenter', '.zoom>div', () => {
+  close = false;
 });
 
-$("body").on("mouseleave",".zoom>div",function(){
-   close=true; 
+$('body').on('mouseleave', '.zoom>div', () => {
+  close = true;
 });
 
-function hideVideo(){
-    if (close) {
-        closeVideo();
-    }
+function hideVideo() {
+  if (close) {
+    closeVideo();
+  }
 }
 
-$("body").append('<div class="zoom" onclick="hideVideo()"><div><aside><img src="assets/images/icons/loader.gif" /></aside><button type="button" class="transition" onclick="closeVideo()"><i class="fa fa-close"></i></button><div><iframe onload="finishLoading()" onerror="finishLoading()" allowfullscreen></iframe></div></div></div>');
+$('body').append('<div class="zoom" onclick="hideVideo()"><div><aside><img src="assets/images/icons/loader.gif" /></aside><button type="button" class="transition" onclick="closeVideo()"><i class="fa fa-close"></i></button><div><iframe onload="finishLoading()" onerror="finishLoading()" allowfullscreen></iframe></div></div></div>');
