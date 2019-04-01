9ae34fff4c1b12eb89c9e1b3d6009fa2e124c8e1
Valentine <valenstical@gmail.com>
2019-04-01 13:22:11 +0100

    fix: fix failing circle ci build

--- newrelic.js
+++ newrelic.js
@@ -3,7 +3,7 @@
  * New Relic agent configuration.
  *
  * See lib/config/default.js in the agent distribution for a more complete
- * description of configuration variables and their potential values
+ * description of configuration variables and their potential values.
  */
 exports.config = {
   /**
